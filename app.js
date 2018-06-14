8c080e19a1a35da627d3b32f001482cbcf9cf4b8
chandu <chandrasr.palle@gmail.com>
2018-06-14 15:49:35 +0200

    render file added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Hi EJS");
+    res.render("home.ejs");
 });
 
 app.listen(3000, function(){
