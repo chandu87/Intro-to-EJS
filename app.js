b0866fb88283e661983d96b6681344faaec2c941
chandu <chandrasr.palle@gmail.com>
2018-06-14 16:08:45 +0200

    passing data to ejs file

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 app.get("/", function(req, res){
     res.render("home.ejs");
 });
+app.get("/lovewith/:name", function(req, res){
+    var loveWithName = req.params.name;
+    res.render("love.ejs", {loves : loveWithName});
+});
 
 app.listen(3000, function(){
 console.log("Server Started at : 3000");
