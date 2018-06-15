0d6c7824bbf7deb37531ea1147fa333b5389a32e
chandu <chandrasr.palle@gmail.com>
2018-06-15 15:49:52 +0200

    pass array of objects data and display using ejs looping

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ app.get("/lovewith/:name", function(req, res){
     var loveWithName = req.params.name;
     res.render("love.ejs", {loves : loveWithName});
 });
-
+app.get("/posts", function(req, res){
+    const postData = [
+                        {title : "Premadesham", hero : "abbas"},
+                        {title : "Nani", hero : "Mahesh Babu"},
+                        {title: "Ganatha Garage", hero :"NTR"}
+                    ];
+    res.render("posts.ejs", {posts : postData});
+});
 app.listen(3000, function(){
 console.log("Server Started at : 3000");
 });
